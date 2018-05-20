a45bfee307dd4bf958ea0fba36d7762174688047
Victor Rafael <contato@victorafael.com>
2018-05-20 14:02:55 -0300

    Base Control Done
    Signed-off-by: Victor Rafael <contato@victorafael.com>

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour {
+	public float inputX;
+
+	private Gorilla gorilla;
+
+	[Header("Buttons")]
+	public string jump;
+	public string attack;
+	public string hitChest;
+
+	void Start(){
+		gorilla = GetComponent<Gorilla> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		inputX = Input.GetAxis ("Horizontal");
+		if (Input.GetButtonDown (jump)) {
+			gorilla.JumpPressed ();
+		}
+		if (Input.GetButtonDown (attack)) {
+			gorilla.Attack ();
+		}
+		if (Input.GetButtonDown (hitChest)) {
+			gorilla.HitChest (true);
+		}
+		if (Input.GetButtonUp (hitChest)) {
+			gorilla.HitChest (false);
+		}
+	}
+}
---
+++ Assets/Scripts/Gorilla.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gorilla : MonoBehaviour {
+	#region Inspector Fields
+	[Header("Behaviour")]
+	public float movementSpeed;
+	public float jumpSpeed;
+	
+	[Header("Components")]
+	[SerializeField] private Rigidbody2D _rigidBody;
+	[SerializeField] private Animator _animator;
+	[SerializeField] private Controls _controls;
+	#endregion
+
+	private bool __grounded;
+	public bool Grounded{
+		get{
+			return __grounded;
+		}
+		set{
+			if (__grounded != value) {
+				__grounded = value;
+				_animator.SetBool ("grounded", value);
+			}
+		}
+	}
+
+	private bool facingRight = true;
+	
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Vector2 velocity = _rigidBody.velocity;
+		float ySpeed = velocity.y;
+		Grounded = ySpeed == 0;
+
+		if (!Grounded) {
+			_animator.SetFloat ("ySpeed", ySpeed);
+		}
+
+		velocity.x = _controls.inputX * movementSpeed;
+		if ((velocity.x < 0 && facingRight) || (velocity.x > 0 && !facingRight)) {
+			facingRight = !facingRight;
+			transform.localScale = new Vector3 (facingRight ? 1 : -1, 1, 1);
+		}
+
+		_animator.SetBool ("moving", velocity.x != 0);
+
+		_rigidBody.velocity = velocity;
+	}
+
+	public void JumpPressed(){
+		if (Grounded) {
+			_rigidBody.velocity = new Vector2 (_rigidBody.velocity.x, jumpSpeed);
+		}
+	}
+	public void Attack(){
+		_animator.SetTrigger ("attack");
+	}
+	public void HitChest(bool state){
+		if (Grounded) {
+			_animator.SetBool ("hitChest", state);
+		}
+	}
+}
